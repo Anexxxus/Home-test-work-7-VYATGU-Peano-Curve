1d34284eb52c3eef4a7f2505f97404865797d719
Konstantin Borisov <68741206+Anexxxus@users.noreply.github.com>
2023-01-25 13:05:17 +0300

    Add files via upload

---
+++ fractal.pas
@@ -0,0 +1,94 @@
+﻿unit fractal;
+interface
+uses GraphABC;
+ var h,x,y:integer;
+procedure lined(dx,dy:integer);
+procedure lineUP;
+procedure lineDO;
+procedure lineRI;
+procedure lineLE;
+procedure PU(i: integer);
+procedure PD(i: integer);
+procedure PL(i: integer);
+procedure PR(i: integer);
+
+implementation
+
+procedure lined(dx,dy:integer);
+begin
+x:=x+dx;
+y:=y+dy;
+lineto(x,y);
+end;
+
+procedure lineUP;
+begin
+lined (0,-h);
+end;
+
+procedure lineDO;
+begin
+lined (0,h);
+end;
+
+procedure lineRI;
+begin
+lined (h,0);
+end;
+
+procedure lineLE;
+begin
+lined (-h,0);
+end;
+procedure PU(i: integer);
+begin
+  if i > 0 then begin
+    PR(i - 1);
+    lineUP;
+    PU(i - 1);
+    lineRI;
+    PU(i - 1);
+    lineDO;
+    PL(i - 1);
+  end;
+end;
+
+procedure PD(i: integer);
+begin
+  if i > 0 then begin
+    PL(i - 1);
+    lineDO;
+    PD(i - 1);
+    lineLE;
+    PD(i - 1);
+    lineUP;
+    PR(i - 1);
+  end;
+end;
+
+procedure PL(i: integer);
+begin
+  if i > 0 then begin
+    PD(i - 1);
+    lineLE;
+    PL(i - 1);
+    lineDO;
+    PL(i - 1);
+    lineRI;
+    PU(i - 1);
+  end;
+end;
+
+procedure PR(i: integer);
+begin
+  if i > 0 then begin
+    PU(i - 1);
+    lineRI;
+    PR(i - 1);
+    lineUP;
+    PR(i - 1);
+    lineLE;
+    PD(i - 1);
+  end;
+end;
+end.
---
+++ module.pas
@@ -0,0 +1,36 @@
+﻿uses GraphABC, fractal;
+var y1,x1,n:integer;
+procedure KeyDown(key: integer);
+begin
+  x := windowwidth div 2;
+  y := windowheight div 2;
+  case key of
+    VK_Up: begin y1 := y1 - 50; end;    //Вверх
+    VK_Down: begin y1 += 50; end;       //Вниз
+    VK_Left: begin x1 := x1 - 50; end;  //Влево
+    VK_Right: begin x1 := x1 + 50; end; //Вправо
+    VK_S: if n>1 then n -= 1;           //Уменьшить фрактал
+    VK_W: n += 1;                       //Увеличить фрактал
+    VK_A: if h>5 then h -= 5;           //Уменьшить масштаб
+    VK_D: h += 5;                       //Увеличить масштаб
+  end;
+  x := x + x1;
+  y := y + y1;
+  clearwindow;
+  moveto(x, y);
+  PL(n);
+  redraw;
+end;
+begin
+  SetWindowSize(800, 500);
+  SetWindowCaption('Кривая Пеано');
+  SetPenColor(clred);
+  x := windowwidth div 2;
+  y := windowheight div 2;
+  n := 5;
+  h:=5;
+  moveto(x, y);
+  PL(n);
+  redraw;
+  onKeyDown += keydown;
+end.
